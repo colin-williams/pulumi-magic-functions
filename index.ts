6ae46593534cbf861e15264b529273809ce78868
Colin Williams <colin.williams.seattle@gmail.com>
2021-10-12 16:30:23 -0700

    initial commit

--- index.ts
+++ index.ts
@@ -2,31 +2,31 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 import { ObjectIdentifier } from "aws-sdk/clients/s3";
-import { spawnSync } from "child_process";
+
 // Create an AWS resource (S3 Bucket)
 const dataBucket = new aws.s3.Bucket("rearc-demo-bucket");
-
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 // Export the name of the bucket
 export const bucketName = dataBucket.id;
+const { spawn } = require('child_process');
 
 
 // A handler function that will list objects in the bucket and bulk delete them
 const putObjects: aws.cloudwatch.EventRuleEventHandler = async (
-    event: aws.cloudwatch.EventRuleEvent
-  ) => {
-    const wget = spawnSync("curl -LJO -o /tmp/wget https://raw.githubusercontent.com/yunchih/static-binaries/master/wget");
     //console.log(`stdout: ${stdout}`)
     //console.log(`stdout: ${wget.stdout.toString}`)
-
-    const wgetSpawned = spawnSync("/tmp/wget -r https://download.bls.gov/pub/time.series/pr/ -l 1 -P /tmp");
+    JSDOM.fromURL("https://download.bls.gov/pub/time.series/pr/ ").then((dom:any) => {
+      console.log(dom.serialize());
+    });
     
-    const awsS3Spawn = spawnSync(`aws s3 sync /tmp/download.bls.gov ${bucketName.get}`);
+    
+    //const awsS3Spawn = spawnSync(`aws s3 sync /tmp/download.bls.gov ${bucketName.get}`);
 
-}
 
 const putObjectsSchedule: aws.cloudwatch.EventRuleEventSubscription = aws.cloudwatch.onSchedule(
   "putObjects",
-  "cron(52 19 * * ? *)",
+  "cron(01 23 * * ? *)",
   putObjects
 );
     
