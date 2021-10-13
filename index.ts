305a9702e2194f19811154c9eec45c653f36b040
Colin Williams <colin.williams.seattle@gmail.com>
2021-10-13 00:37:44 -0700

    adding serialization-error for SpawnSync attempt

--- index.ts
+++ index.ts
@@ -2,7 +2,8 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 import { ObjectIdentifier } from "aws-sdk/clients/s3";
-import { spawnSync } from "child_process";
+const { spawnSync } = require('child_process');
+
 // Create an AWS resource (S3 Bucket)
 const dataBucket = new aws.s3.Bucket("rearc-demo-bucket");
 
@@ -14,13 +15,24 @@ export const bucketName = dataBucket.id;
 const putObjects: aws.cloudwatch.EventRuleEventHandler = async (
     event: aws.cloudwatch.EventRuleEvent
   ) => {
-    const wget = spawnSync("curl -LJO -o /tmp/wget https://raw.githubusercontent.com/yunchih/static-binaries/master/wget");
-    //console.log(`stdout: ${stdout}`)
-    //console.log(`stdout: ${wget.stdout.toString}`)
+    const curl = spawnSync("curl -LJO -o /tmp/wget https://raw.githubusercontent.com/yunchih/static-binaries/master/wget");
+
+    curl.stdout.on('data', (data: string) => {
+      console.log(`stdout: ${data}`);
+    });
+    
+    curl.stderr.on('data', (data: string) => {
+      console.error(`stderr: ${data}`);
+    });
+    
+    curl.on('close', (code: number) => {
+      console.log(`child process exited with code ${code}`);
+    });
+
 
     const wgetSpawned = spawnSync("/tmp/wget -r https://download.bls.gov/pub/time.series/pr/ -l 1 -P /tmp");
     
-    const awsS3Spawn = spawnSync(`aws s3 sync /tmp/download.bls.gov ${bucketName.get}`);
+    // const awsS3Spawn = spawnSync(`aws s3 sync /tmp/download.bls.gov ${bucketName.get}`);
 
 }
 
@@ -29,28 +41,3 @@ const putObjectsSchedule: aws.cloudwatch.EventRuleEventSubscription = aws.cloudw
   "cron(52 19 * * ? *)",
   putObjects
 );
-    
-    /*
-    const s3Client = new aws.sdk.S3(); //creates interface to service
-    const bucket = dataBucket.id.get();
-  
-    const { Contents = [] } = await s3Client //get list of objects in bucket
-      .listObjects({ Bucket: bucket })
-      .promise();
-    const objects: ObjectIdentifier[] = Contents.map(object => {
-      return { Key: object.Key! };
-    });
-  
-    await s3Client //delete objects
-      .deleteObjects({
-        Bucket: bucket,
-        Delete: { Objects: objects, Quiet: false }
-      })
-      .promise()
-      .catch(error => console.log(error));
-    console.log(
-      `Deleted ${Contents.length} item${
-        Contents.length === 1 ? "" : "s"
-      } from ${bucket}.`
-    );
-    */
